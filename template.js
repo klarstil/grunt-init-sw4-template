9b00bf8d2ee042af32afda9289ad4b5189a0f052
klarstil <klarstil@googlemail.com>
2013-06-15 13:37:49 +0200

    Refactored: The init task creates the template in a new directory

--- template.js
+++ template.js
@@ -18,15 +18,12 @@ exports.after = 'You should now install project dependencies with _npm ' +
     '\n\n' +
     'http://gruntjs.com/getting-started';
 
-// Any existing file or directory matching this wildcard will cause a warning.
-exports.warnOn = '*';
-
 // The actual init template.
 exports.template = function(grunt, init, done) {
     init.process({ type: 'sw4-template' }, [
 
         // Prompt for these values
-        init.prompt('name'),
+        init.prompt('name', 'emotion_mytemplate'),
         init.prompt('title', function(value, data, done) {
             // Fix Shopware capitalization
             value = value.replace(/shopware/gi, 'Shopware');
@@ -38,13 +35,16 @@ exports.template = function(grunt, init, done) {
         init.prompt('licenses', 'MIT'),
         init.prompt('author_name'),
         init.prompt('author_email'),
-        init.prompt('shopware_version', '4')
+        {
+            name: 'shopware_version',
+            message: 'Min. Shopware version',
+            default: '4.1'
+        }
     ], function(err, props) {
-        var files;
+        var files, newFiles = {};
 
         props.dependencies = { jquery: props.jquery_version || '>= 1.9' };
-        props.keywords = [];
-
+        props.keywords = [ 'shopware' ];
 
         // Files to copy (and process).
         files = init.filesToCopy(props);
@@ -52,15 +52,21 @@ exports.template = function(grunt, init, done) {
         // Add properly-named license files.
         init.addLicenseFiles(files, props.licenses);
 
+        // Modifiy the destpath to create the template in a new directory named by the project name
+        Object.keys(files).forEach(function(destpath) {
+            newFiles[props.name + '/' + destpath] = files[destpath];
+        });
+
+        init.copyAndProcess(newFiles, props);
+
         // Generate package.json file, used by npm and grunt.
-        init.writePackageJSON('package.json', {
+        init.writePackageJSON(props.name + '/package.json', {
             name: 'sw4-frontend-template',
             version: '0.0.0-ignored',
             npm_test: 'grunt qunit',
             node_version: '>= 0.8.0',
             devDependencies: {
                 'grunt-contrib-jshint': '~0.1.1',
-                'grunt-contrib-qunit': '~0.1.1',
                 'grunt-contrib-concat': '~0.1.2',
                 'grunt-contrib-uglify': '~0.1.1',
                 'grunt-contrib-watch': '~0.2.0',
